df419a4c7aafd667e450e76992faf211dee08d78
eligifty-coder <gift.weike@gmail.com>
2026-08-18 23:44:01 +0100

    react strict mode

---
+++ 05_components_props_jsx/33_rendering_root_components/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from  "react-dom/client"
+
+function App(){
+    return <h1>Hello React!</h1>
+}
+
+// React v18
+const root =  ReactDom.createRoot(document.getElementById("root"))
+root.render(<React.StrictMode>
+    <App/>
+</React.StrictMode>)
