2a6adeed2be08f2e955e48a17f5eae11161ed4c7
eligifty-coder <gift.weike@gmail.com>
2026-08-25 17:29:38 +0100

    rendering list

--- 05_components_props_jsx/46_rendering_list/src/index.jsx
+++ 05_components_props_jsx/46_rendering_list/src/index.jsx
@@ -71,35 +71,36 @@ function Header(){
 function Menu(){
     return <main className="menu">
             <h2  >Our Menu</h2>
-            <Pizza
-            name='Pizza Spinaci'
-            ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-            photoName= "pizzas/spinaci.jpg"
-            price={10}
-            />
-            <Pizza
-            name='Pizza Funghi'
-            ingredients="Tomato, mushroom"
-            price={12}
-            photoName= "pizzas/funghi.jpg"
-            />
+            <ul className="pizzas">
+                {
+                    pizzaData.map(pizza=> (
+                        <Pizza
+                        key={pizza.name}
+                        pizzaObj={pizza}
+                        />
+                    ) )
+                }
+            </ul>
+            
     </main>
+}
+
+
+function Pizza(props) {
+    return <li className="pizza">
+        <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}  />
+        <div>
+            <h3>{props.pizzaObj.name} </h3>
+            <p>{props.pizzaObj.ingredients}</p>
+            <span>{props.pizzaObj.price +3}</span>
+        </div>
+    </li>;
 }
 function Footer(){
     return <footer className="footer"> {new Date().toLocaleTimeString()}. We're currently open</footer>
 }
 
-function Pizza(props) {
-    console.log(props)
-    return <div className="pizza">
-        <img src={props.photoName} alt={props.name}  />
-        <div>
-            <h3>{props.name} </h3>
-            <p>{props.ingredients}</p>
-            <span>{props.price +3}</span>
-        </div>
-    </div>;
-}
+
 // React v18
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
