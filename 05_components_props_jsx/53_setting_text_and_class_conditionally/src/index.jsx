dfe69f80621811ee986bbf1997b8309a867b5404
eligifty-coder <gift.weike@gmail.com>
2026-08-27 10:48:18 +0100

    setting text and classes conditionally

--- 05_components_props_jsx/53_setting_text_and_class_conditionally/src/index.jsx
+++ 05_components_props_jsx/53_setting_text_and_class_conditionally/src/index.jsx
@@ -96,12 +96,13 @@ function Menu(){
 
 
 function Pizza({pizzaObj}) {
-    return <li className="pizza">
+    return <li className={`pizza ${pizzaObj.soldOut? 'sold-out': ''}`}>
         <img src={pizzaObj.photoName} alt={pizzaObj.name}  />
         <div>
             <h3>{pizzaObj.name} </h3>
             <p>{pizzaObj.ingredients}</p>
-            <span>{pizzaObj.price +3}</span>
+            {/* {pizzaObj.soldOut? <span>SOLD OUT</span> : <span>{pizzaObj.price}</span>} */}
+            <span>{pizzaObj.soldOut? 'SOLD OUT' : pizzaObj.price +3 }</span>
         </div>
     </li>;
 }
